d13d0a1c776c7a682619688bcfc49a5641a57540
craigbauer89 <craigbauer89@gmail.com>
2022-05-10 17:14:39 +0200

    Update 2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-var risposta = prompt('Per favore inserisci un numero piu di 10 e pari' );
+let risposta = prompt('Per favore inserisci un numero piu di 10 e pari' );
 console.log('Prima risposta:',risposta)
 
-if (risposta > 10 && risposta % 10 == 0 ) {
+if (risposta > 9 && risposta % 10 == 0 ) {
 
-    document.getElementById('risposta').innerHTML = 'Grazie. Questa è un numero pari.';
+    document.getElementById('risposta').innerHTML = 'Grazie. Questo numero  è un numero pari.';
 } else {
     
-    risposta = prompt('Scusa. Questa è un numero dispari.' );
+    risposta = prompt('Scusa. Questo numero è un numero dispari o e meno di 10. Inserisci un altro' );
     console.log('Seconda risposta:',risposta)
-    if (risposta < 10 && risposta % 10 != 0) {
+    if (risposta < 10 || risposta % 10 != 0) {
 
         document.getElementById('risposta').innerHTML = 'Mi spiace. Tentativi scaduti';
     } else {
-        document.getElementById('risposta').innerHTML = 'Grazie. Questa è un numero pari.';
+        document.getElementById('risposta').innerHTML = 'Grazie. Questo nummero  è un numero pari.';
     }
 }
 document.getElementById('risposta modificata').innerHTML = risposta;
